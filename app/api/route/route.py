f86e53cf43c8b06c01ea2adf995de10cbf184da7
JOKER_SON <alreadysons@gmail.com>
2024-11-06 16:30:27 +0900

    ♻️ API 불러오는 route 비동기 방식으로 수정

--- app/api/route/route.py
+++ app/api/route/route.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter , HTTPException
-from app.service.request import get_spoonacular,get_edam
+from app.service.request import get_recipe_api
 from app.service.scrap import get_recipe_scrap
+from app.service.parse import parse_data
 from app.dto.data_transfer_object import RecipeService
 from app.model.recipe_model import Recipe
 from typing import List
@@ -10,20 +11,18 @@ router = APIRouter()
 recipe_service = RecipeService()
 
 
-@router.get("/recipes/{query}")
-def get_recipes(query: str):
-    return get_spoonacular(query)
-
-@router.get("/recipes2/{query}")
-def get_recipes2(query:str):
-    return get_edam(query)
+@router.get("/recipes/{start}/{end}")
+async def get_recipes(start:int,end:int):
+    data = await get_recipe_api(start,end)
+    recipes = parse_data(data)
+    return recipes
 
 @router.get("/scrap")
 async def get_scrap() :
     max_attempts = 5
     for _ in range(max_attempts):
         recipe_id = random.randint(6803892, 7037297)  # 랜덤 ID 생성
-        recipe_data = get_recipe_scrap(recipe_id)
+        recipe_data = await get_recipe_scrap(recipe_id)
         
         # 유효한 레시피 데이터가 있으면 반환
         if "error" not in recipe_data:
