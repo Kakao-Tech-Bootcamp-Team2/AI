d5624094e03518a9a84fe14c7e34fcee9519b055
JOKER_SON <alreadysons@gmail.com>
2024-11-28 16:52:40 +0900

    ♻️ embedding_model 부분 리팩토링

--- app/service/preprocess/data_embedding.py
+++ app/service/preprocess/data_embedding.py
@@ -1,8 +1,8 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 class EmbeddingService:
     def __init__(self):
-        model_name = "intfloat/multilingual-e5-large-instruct"
-        self.embedding_model = HuggingFaceEmbeddings(model_name=model_name)
+        self.model_name = "intfloat/multilingual-e5-large-instruct"
+        self.embedding_model = HuggingFaceEmbeddings(model_name=self.model_name)
 
     def embed_text(self, texts):
         return self.embedding_model.embed_documents(texts)
