77696f11d24c70a1319c3208237bb3c36c00e1a3
JOKER_SON <alreadysons@gmail.com>
2024-12-05 17:42:17 +0900

    ♻️ 필터링 제거

--- app/service/search/search.py
+++ app/service/search/search.py
@@ -2,7 +2,7 @@ from typing import List, Dict
 from app.repositorie.db_connection import DatabaseConnection
 from app.service.preprocess.data_embedding import EmbeddingService
 
-def search_recipes_by_text(query: str, top_k: int = 1000) -> List[Dict]:
+def search_recipes_by_text(query: str, top_k: int = 50) -> List[Dict]:
     # 쿼리 전처리
     if "," not in query:
         query_ingredients = [query.strip()]
@@ -21,10 +21,10 @@ def search_recipes_by_text(query: str, top_k: int = 1000) -> List[Dict]:
     
     return search_recipes(query_embedding, query_ingredients, top_k)
 
-def search_recipes(query_embedding: List[float], query_ingredients: List[str], top_k: int = 1000) -> List[Dict]:
+def search_recipes(query_embedding: List[float], query_ingredients: List[str], top_k: int = 50) -> List[Dict]:
     db = DatabaseConnection()
     
-    #디버깅 로그
+    # 디버깅 로그
     print(f"\n=== 검색 시작 ===")
     print(f"검색할 재료: {query_ingredients}")
     print(f"임베딩 벡터 샘플: {query_embedding[:5]}...")  # 처음 5개 값만 출력
@@ -42,29 +42,30 @@ def search_recipes(query_embedding: List[float], query_ingredients: List[str], t
         print("검색 결과가 없습니다.")
         return []
 
+    # 'score'를 기준으로 매칭 결과 정렬
+    sorted_matches = sorted(results["matches"], key=lambda x: x['score'], reverse=True)
+    
     filtered_results = []
-    for match in results["matches"]:
+
+    for match in sorted_matches:
         match_id = match['id'] 
         match_ingredients = match["metadata"].get("ingredients", [])
         match_title = match["metadata"].get("title", "")
-        raw_ingredients = match["metadata"].get("raw_ingredients", [])  # raw_ingredients 추가
+        raw_ingredients = match["metadata"].get("raw_ingredients", [])
         print(f"\n매칭 항목:")
         print(f"id : {match_id}")
         print(f"제목: {match_title}")
         print(f"재료: {match_ingredients}")
         print(f"날것의 재료: {raw_ingredients}")
         print(f"점수: {match['score']}")
-
-        if all(ingredient in match_ingredients for ingredient in query_ingredients):
-            print(f"✅ 필터 통과: {match_title}")
-            filtered_results.append({
-                "id" : match_id,
-                "title": match_title,
-                "ingredients": raw_ingredients,
-                "steps": match["metadata"].get("steps", [])
-            })
-        else:
-            print(f"❌ 필터 실패: {match_title}")
+        
+        # 결과 딕셔너리에 'score'를 포함하지 않고 필요한 정보만 추가
+        filtered_results.append({
+            "id" : match_id,
+            "title": match_title,
+            "ingredients": raw_ingredients,
+            "steps": match["metadata"].get("steps", [])
+        })
 
     print(f"\n=== 최종 결과 ===")
     print(f"필터링된 결과 수: {len(filtered_results)}")
